d58958d2a3930ded226bf601fccb27c6ae90eb83
STAGR005 <Robert.Stagner@Disney.com>
2014-03-14 15:18:53 -0700

    * corrected typo in JUnit runnter dotcucumber was misspelled

--- src/test/groovy/RunCucumberITCase.groovy
+++ src/test/groovy/RunCucumberITCase.groovy
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith
         , features = ["classpath:features"]
         , glue = ["src/test/groovy/cucumber/steps"]
         , tags = ["~@manual"]
-        , dotcumber = ".cucumber"
+        , dotcucumber = ".cucumber"
 )
 class RunCucumberITCase {
 }
