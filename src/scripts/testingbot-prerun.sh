6118e65b0b217830cafb0287323422628ce1ad4b
Robert Stagner <robert.stagner@disney.com>
2022-02-25 10:44:42 -0800

    updated shell script with curl command

--- src/scripts/testingbot-prerun.sh
+++ src/scripts/testingbot-prerun.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-wget -P /tmp https://github.com/restagner/cucumber-groovy-selenium/blob/master/src/test/resources/images/flower.png
+# wget -P /tmp https://github.com/restagner/cucumber-groovy-selenium/blob/master/src/test/resources/images/flower.png
+curl -h
