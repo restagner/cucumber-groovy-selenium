91d29671c9904ec96fdeb145c1073b61ccda4500
Robert Stagner <robert.stagner@disney.com>
2022-02-25 10:49:55 -0800

    updated prerun shell script to print message and execute "ls" command

--- src/scripts/testingbot-prerun.sh
+++ src/scripts/testingbot-prerun.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # wget -P /tmp https://github.com/restagner/cucumber-groovy-selenium/blob/master/src/test/resources/images/flower.png
-curl -h
+printf "Hi from Prerun Shell Script\n"
+ls .
